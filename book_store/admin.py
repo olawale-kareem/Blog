85c2a04e4744737235caddb80488947e1881ae10
olawale-kareem <olawale.kareem@decagon.dev>
2021-09-15 03:32:24 +0100

    admin-setup and registering the Books model

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Books
 
-# Register your models here.
+class BooksAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)                 #if you wanna make the field read only
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'rating')
+    list_display =('title', 'author', 'rating')
+
+admin.site.register(Books,BooksAdmin)
+
